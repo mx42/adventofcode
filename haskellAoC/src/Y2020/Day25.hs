d040b97bc8186b4ed261c901e770eccb2a3cc885
Xavier Morel <xavier.morel@cybelangel.com>
2021-01-10 10:47:08 +0100

    Add 2020 day 25 p1

--- haskellAoC/src/Y2020/Day25.hs
+++ haskellAoC/src/Y2020/Day25.hs
@@ -1,6 +1,20 @@
 module Y2020.Day25 (y20day25) where
 
+doTransform :: Int -> Int -> Int
+doTransform subjNumber n = rem (n * subjNumber) 20201227
+
+getLoopSize :: Int -> Int
+getLoopSize targetPK = length $ takeWhile (/= targetPK) $ iterate (doTransform 7) 1
+
+transformNtimes :: Int -> Int -> Int
+transformNtimes subjNumber loopSize = head $ drop loopSize $ iterate (doTransform subjNumber) 1
+
 y20day25 :: [String] -> (String, String)
 y20day25 input = (part1, part2)
-  where part1  = "WIP"
-        part2  = "WIP"
+  where part1  = show $ transformNtimes doorpk cardls
+        part2  = show $ "WIP"
+        input' = map read input :: [Int]
+        doorpk = input' !! 0
+        cardpk = input' !! 1
+        -- doorls = getLoopSize doorpk
+        cardls = getLoopSize cardpk
